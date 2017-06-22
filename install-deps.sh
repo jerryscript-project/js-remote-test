c546668c9ce7c4413a19a86b9043b1e0ef023d37
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2017-06-22 13:03:55 +0200

    Add missing dependency

--- install-deps.sh
+++ install-deps.sh
@@ -4,7 +4,7 @@
 sudo apt-get install -y autoconf libtool gperf flex bison autoconf2.13
 sudo apt-get install -y cmake libncurses-dev libusb-1.0-0-dev genromfs
 sudo apt-get install -y libsgutils2-dev gcc-arm-none-eabi minicom
-sudo apt-get install -y python-pip pkg-config
+sudo apt-get install -y python-pip pkg-config libssl-dev
 sudo apt-get install -y gcc-arm-linux-gnueabihf binutils-arm-linux-gnueabi
 
 sudo pip install schedule paramiko
