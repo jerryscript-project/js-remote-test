20b7692847b24c157b6b7ea7b31825e7fc59f888
Roland Takacs <rtakacs@inf.u-szeged.hu>
2018-03-14 13:57:15 +0100

    Add missing module import for the ARTIK530 builder (#125)
    JSRemoteTest-DCO-1.0-Signed-off-by: Roland Takacs rtakacs.uszeged@partner.samsung.com

--- API/builder/targets/artik530.py
+++ API/builder/targets/artik530.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from API.common import utils
+from API.common import paths, utils
 from API.builder import builder
 
 
