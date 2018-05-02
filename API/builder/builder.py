c1dee24293f1fd82ca54cde043317ac1c02090a1
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2018-05-02 18:30:18 +0200

    Copy the dynamic module test folder to the devices only in case of IoT.js (#143)
    JSRemoteTest-DCO-1.0-Signed-off-by: Robert Sipka rsipka.uszeged@partner.samsung.com

--- API/builder/builder.py
+++ API/builder/builder.py
@@ -94,6 +94,9 @@ class BuilderBase(object):
         dynamic_folder = utils.join(builddir, 'test')
 
         utils.copy(application['paths']['libdir'], lib_folder)
-        utils.copy(application['paths']['dynamic-module-test'], dynamic_folder)
+
+        if application == 'iotjs':
+            utils.copy(application['paths']['dynamic-module-test'], dynamic_folder)
+
         utils.copy(target_module['paths']['linker-map'], linker_map)
         utils.copy(target_module['paths']['image'], builddir)
