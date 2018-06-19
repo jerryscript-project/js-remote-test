65b22f04757a40baa7b55082df7eabf294cefee1
Peter Marki <marpeter@inf.u-szeged.hu>
2018-06-19 13:10:30 +0200

    Create a pylintrc file for style checking and improving code quality (#166)
    JSRemoteTest-DCO-1.0-Signed-off-by: Peter Marki marpeter@inf.u-szeged.hu

--- install-deps.sh
+++ install-deps.sh
@@ -23,7 +23,7 @@ fi
 sudo apt-get install -y autoconf libtool gperf flex bison autoconf2.13
 sudo apt-get install -y cmake libncurses-dev libusb-1.0-0-dev genromfs
 sudo apt-get install -y libsgutils2-dev gcc-arm-none-eabi minicom
-sudo apt-get install -y python-pip pkg-config libssl-dev
+sudo apt-get install -y python-pip pylint pkg-config libssl-dev
 sudo apt-get install -y gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi
 sudo apt-get install -y binutils-arm-linux-gnueabi
 sudo apt-get install -y mosquitto
